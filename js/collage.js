9c266564628e68f510a55c44aa3de8768b90d789
AnneDyuzh <anyutka.dyuzheva@mail.ru>
2024-02-19 23:53:56 +0300

    Add_Comments

--- js/collage.js
+++ js/collage.js
@@ -4,25 +4,26 @@ const modalImg = document.querySelector('.modal_window_img');
 const buttonClose = document.querySelector('.button_close');
 const buttonDwnld = document.querySelector('.button_dwnld');
 const url = "https://dog.ceo/api/breeds/image/random";
-let images = [];
+let images = []; //пути изображений
 let timer;
 let numberImg = 12;
 
+//Получаем изображения через зарос рандомно по одному
 function getImage() {
     fetch(url)
         .then(response => { return response.json(); })
         .then(data => {
-            images.unshift(data.message.toString());
+            images.unshift(data.message.toString()); //добавляем сразу в массив с путями изображений
         })
         .catch(error => {
             console.error(error);
         })
 }
 
-//добавляет  фотки в коллаж
+//Добавляем изображения в коллаж
 function addNewImageToCollage() {
-    getImage();
-    if (images.length > numberImg) {
+    getImage(); //Получаем изображение
+    if (images.length > numberImg) { //Изображения будут меняться по часовой стрелки за счёт удаления последнего
         images.pop();
     }
     collage.innerHTML = '';
@@ -30,6 +31,7 @@ function addNewImageToCollage() {
         const img = document.createElement('img');
         img.classList.add("img_collage");
         img.src = imgSrc;
+        //Ширину изображения получаем рандомно, высота через css auto, чтобы сохранить пропорции
         let imgWidth = parseInt(Math.random() * 450);
         img.width = `${imgWidth}`;
         collage.appendChild(img);
@@ -37,12 +39,15 @@ function addNewImageToCollage() {
     });
 };
 
+//Обновление коллажа срабатывает с интервалом в 3 секунды
 document.addEventListener("DOMContentLoaded", () => {
     timer = setInterval(function () {
         addNewImageToCollage();
     }, 3000);
 });
 
+/* Появление модального окна с изображением (останавливаем обновление галлереи), для скачивания используем тэг a с downoload, в 
+src, которого вставляем полученный путь картинки*/
 collage.addEventListener('click', event => {
     if (event.target.tagName === 'IMG') {
         const imgModal = document.createElement('img');
@@ -54,6 +59,7 @@ collage.addEventListener('click', event => {
     }
 });
 
+//Закрвтие модального окна и возобновление работы коллажа
 buttonClose.addEventListener('click', event => {
     modalWindow.style.display = "none";
     modalImg.innerHTML = '';
